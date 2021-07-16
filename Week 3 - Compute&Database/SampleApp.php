5a442845d58c5a6d8b873722db86fe862a5c2331
Nathachai <saimadma@gmail.com>
2021-07-17 02:12:11 +0700

    Add Bootstrap/Add DB Info File

--- Week 3 - Compute&Database/SampleApp.php
+++ Week 3 - Compute&Database/SampleApp.php
@@ -1,82 +1,110 @@
 
-<?php include "../inc/dbinfo.inc"; ?>
-<html>
-<body>
-<h1>Sample page</h1>
+<?php 
+#include "../inc/dbinfo.inc"; 
+?>
+<!DOCTYPE html>
+
+<html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Employee Information | Sample App</title>
+      <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  </head>
+  <body>
+    <div class="container">
+    <div class="jumbotron"><h1>🧑‍💻 Employee Information | Sample App</h1></div>
 <?php
+  
 
   /* Connect to MySQL and select the database. */
-  $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
+  // $connection = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
 
-  if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  // if (mysqli_connect_errno()) echo "Failed to connect to MySQL: " . mysqli_connect_error();
 
-  $database = mysqli_select_db($connection, DB_DATABASE);
+  // $database = mysqli_select_db($connection, DB_DATABASE);
 
   /* Ensure that the EMPLOYEES table exists. */
-  VerifyEmployeesTable($connection, DB_DATABASE);
+  // VerifyEmployeesTable($connection, DB_DATABASE);
 
   /* If input fields are populated, add a row to the EMPLOYEES table. */
-  $employee_name = htmlentities($_POST['NAME']);
-  $employee_address = htmlentities($_POST['ADDRESS']);
+  // $employee_name = htmlentities($_POST['NAME']);
+  // $employee_address = htmlentities($_POST['ADDRESS']);
 
-  if (strlen($employee_name) || strlen($employee_address)) {
-    AddEmployee($connection, $employee_name, $employee_address);
-  }
+  // if (strlen($employee_name) || strlen($employee_address)) {
+  //   AddEmployee($connection, $employee_name, $employee_address);
+  // }
+  
 ?>
 
-<!-- Input form -->
-<form action="<?PHP echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
-  <table border="0">
-    <tr>
-      <td>NAME</td>
-      <td>ADDRESS</td>
-    </tr>
-    <tr>
-      <td>
-        <input type="text" name="NAME" maxlength="45" size="30" />
-      </td>
-      <td>
-        <input type="text" name="ADDRESS" maxlength="90" size="60" />
-      </td>
-      <td>
-        <input type="submit" value="Add Data" />
-      </td>
-    </tr>
-  </table>
-</form>
+    <!-- Input form -->
+    <form action="<?PHP echo $_SERVER['SCRIPT_NAME'] ?>" method="POST">
+      <div class="form-group row" style="width:100%;margin:0;">
+        <div class="col">
+        <label for="NAME">Name:</label>
+        <input class="form-control" type="text" name="NAME"/>
+        </div>
 
-<!-- Display table data. -->
-<table border="1" cellpadding="2" cellspacing="2">
-  <tr>
-    <td>ID</td>
-    <td>NAME</td>
-    <td>ADDRESS</td>
-  </tr>
+        <div class="col">
+        <label for="ADDRESS">Salary:</label>
+        <input class="form-control" type="text" name="SALARY"/>
+        </div>
 
-<?php
+        <div class="col">
+        <label for="JOB">Job:</label>
+        <select class="form-control" name="JOB">
+          <option>Officer</option>
+          <option>Developer</option>
+          <option>Secretary</option>
+          <option>Project Manager</option>
+        </select>
 
-$result = mysqli_query($connection, "SELECT * FROM EMPLOYEES");
+        </div>
+        <input style="height: 60%; margin-top: auto;" class="btn btn-primary" type="submit" value="Add Data" />
+      </div>
+    </form>
 
-while($query_data = mysqli_fetch_row($result)) {
-  echo "<tr>";
-  echo "<td>",$query_data[0], "</td>",
-       "<td>",$query_data[1], "</td>",
-       "<td>",$query_data[2], "</td>";
-  echo "</tr>";
-}
-?>
+    <!-- Display table data. -->
+    <div style="padding: 10px;">
+      <table class="table table-hover" >
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NAME</th>
+            <th>SALARY</th>
+            <th>JOB</th>
+          </tr>
+        <thead>
+        <tbody>
+          <?php
 
-</table>
+          // $result = mysqli_query($connection, "SELECT * FROM EMPLOYEES");
+          
+          // while($query_data = mysqli_fetch_row($result)) {
+          //   echo "<tr>";
+          //   echo "<td>",$query_data[0], "</td>",
+          //        "<td>",$query_data[1], "</td>",
+          //        "<td>",$query_data[2], "</td>";
+          //        "<td>",$query_data[3], "</td>";
+          //   echo "</tr>";
+          // }
+          ?>
+        </tbody>
+      </div>
+    </table>
 
 <!-- Clean up. -->
 <?php
 
-  mysqli_free_result($result);
-  mysqli_close($connection);
+  // mysqli_free_result($result);
+  // mysqli_close($connection);
 
 ?>
 
-</body>
+  </div>
+  </body>
 </html>
 
 
